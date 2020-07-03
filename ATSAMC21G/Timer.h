923ac51b4162edf8ef0a73d90227426299089bcb
Balazs Racz <balazs.racz@gmail.com>
2020-07-03 22:01:16 +0200

    Updates the clock setting code for SAMC21.

--- ATSAMC21G/Timer.h
+++ ATSAMC21G/Timer.h
@@ -1,5 +1,5 @@
-#ifndef ATSAMD21GTimer_h
-#define ATSAMD21GTimer_h
+#ifndef ATSAMC21GTimer_h
+#define ATSAMC21GTimer_h
 
 #include "../VirtualTimer.h"
 #include <Arduino.h>
@@ -26,36 +26,20 @@ public:
 
     void initialize() {
         if(timer == TCC0 || timer == TCC1) {
-            REG_GCLK_GENDIV =   GCLK_GENDIV_DIV(1) |            // Divide 48MHz by 1
-                                GCLK_GENDIV_ID(4);              // Apply to GCLK4
-            while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
-                
-            REG_GCLK_GENCTRL =  GCLK_GENCTRL_GENEN |            // Enable GCLK
-                                GCLK_GENCTRL_SRC_DFLL48M |      // Set the 48MHz clock source
-                                GCLK_GENCTRL_ID(4);             // Select GCLK4
-            while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
-            
-            REG_GCLK_CLKCTRL =  GCLK_CLKCTRL_CLKEN |            // Enable generic clock
-                                4 << GCLK_CLKCTRL_GEN_Pos |     // Apply to GCLK4
-                                GCLK_CLKCTRL_ID_TCC0_TCC1;      // Feed GCLK to TCC0/1
-            while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+            MCLK->APBCMASK.bit.TCC0_ = 1;
+            MCLK->APBCMASK.bit.TCC1_ = 1;
+            GCLK->GENCTRL[4].reg = ( GCLK_GENCTRL_DIV(2) | GCLK_GENCTRL_SRC_DPLL96M | GCLK_GENCTRL_IDC | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_OE );
+            while ((GCLK->SYNCBUSY.bit.GENCTRL >> 4)  & 1); // Wait for synchronization
+            GCLK->PCHCTRL[28].reg = ( GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN(4) );  // 28 = TCC0_TCC1
+            while ((GCLK->SYNCBUSY.bit.GENCTRL >> 4) & 1); // Wait for synchronization
         } 
         else if (timer == TCC2) {
-            REG_GCLK_GENDIV =   GCLK_GENDIV_DIV(1) |            // Divide 48MHz by 1
-                                GCLK_GENDIV_ID(5);              // Apply to GCLK4
-            while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
-                
-            REG_GCLK_GENCTRL =  GCLK_GENCTRL_GENEN |            // Enable GCLK
-                                GCLK_GENCTRL_SRC_DFLL48M |      // Set the 48MHz clock source
-                                GCLK_GENCTRL_ID(5);             // Select GCLK4
-            while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
-            
-            REG_GCLK_CLKCTRL =  GCLK_CLKCTRL_CLKEN |            // Enable generic clock
-                                5 << GCLK_CLKCTRL_GEN_Pos |     // Apply to GCLK4
-                                GCLK_CLKCTRL_ID_TCC2_TC3;      // Feed GCLK to TCC0/1
-            while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+            MCLK->APBCMASK.bit.TCC2_ = 1;
+            GCLK->GENCTRL[5].reg = ( GCLK_GENCTRL_DIV(2) | GCLK_GENCTRL_SRC_DPLL96M | GCLK_GENCTRL_IDC | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_OE );
+            while ((GCLK->SYNCBUSY.bit.GENCTRL >> 5)  & 1); // Wait for synchronization
+            GCLK->PCHCTRL[29].reg = ( GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN(5) ); // 29 = TCC2
+            while ((GCLK->SYNCBUSY.bit.GENCTRL >> 5) & 1); // Wait for synchronization
         }
-        
         
         timer->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;             // Select NPWM as waveform
         while (timer->SYNCBUSY.bit.WAVE);                    // Wait for synchronization
@@ -67,6 +51,7 @@ public:
         lastMicroseconds = microseconds;
         
         const unsigned long cycles = F_CPU / 1000000 * microseconds;    // cycles corresponds to how many clock ticks per microsecond times number of microseconds we want
+        timer->CTRLA.bit.PRESCALER = 0;
         if(cycles < timer_resolution) {
             timer->CTRLA.reg |= TCC_CTRLA_PRESCALER(TCC_CTRLA_PRESCALER_DIV1_Val);
             pwmPeriod = cycles;
@@ -141,4 +126,4 @@ extern Timer TimerC;
 
 
 
-#endif
+#endif // ATSAMC21GTimer_h
